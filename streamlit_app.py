ce7016bae83f5faa0de91a20d5f60773af91191b
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-06-14 15:39:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,23 @@
-import streamlit
+import streamlit as st
+import requests
+
+st.title("Text Classification with Hugging Face Model")
+
+text_input = st.text_area("Enter text for classification:")
+
+if st.button("Classify"):
+    if text_input:
+        # Replace the URL with your Azure FastAPI backend URL
+        url = "https://api-projet-7.azurewebsites.net/predict"
+        response = requests.post(
+            url,
+            json={"text": text_input},
+            headers={"Content-Type": "application/json"},
+        )
+        if response.status_code == 200:
+            result = response.json()
+            st.write(f"Predicted Class ID: {result['predicted_class_id']}")
+        else:
+            st.write("Error in classification request")
+    else:
+        st.write("Please enter some text for classification")
