daeec5a9358919a1607d25d913d96697aacb9b18
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-06-16 09:05:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+import streamlit as st
+import requests
+
 st.title("Projet 7 : Réalisez une analyse de sentiments grâce au Deep Learning")
 
 text_input = st.text_area("Entrez le texte dont vous souhaitez analyser le sentiment :")
@@ -13,7 +16,7 @@ if st.button("Analyser"):
         )
         if response.status_code == 200:
             result = response.json()
-            predicted_class_id = result
+            predicted_class_id = result['predicted_class_id']
             sentiment = "positif" if predicted_class_id == 1 else "négatif"
             st.write(f"Le sentiment prédit est : {sentiment}")
         else:
