5af94b3ce1ea69d54f38decb1548a406ec342151
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-06-30 11:56:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,20 @@ logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=55f8c386
 
 st.title("Projet 7 : Réalisez une analyse de sentiments grâce au Deep Learning")
 
-# Function to reset the session state
-def reset_session():
-    for key in st.session_state.keys():
-        del st.session_state[key]
-
 # Initialize session state variables if they don't exist
 if 'analyze_button_clicked' not in st.session_state:
     st.session_state['analyze_button_clicked'] = False
 if 'sentiment' not in st.session_state:
     st.session_state['sentiment'] = ""
+if 'feedback_submitted' not in st.session_state:
+    st.session_state['feedback_submitted'] = False
 
 text_input = st.text_area("Entrez le texte dont vous souhaitez analyser le sentiment :")
 
 if st.button("Analyser"):
     if text_input:
         st.session_state['analyze_button_clicked'] = True
+        st.session_state['feedback_submitted'] = False
         # Remplacer l'URL par celle de votre backend FastAPI Azure
         url = "https://api-projet-7.azurewebsites.net/predict"
         response = requests.post(
@@ -36,15 +34,15 @@ if st.button("Analyser"):
             result = response.json()
             predicted_class_id = result[0]
             sentiment = "positif" if predicted_class_id == 1 else "négatif"
-            st.write(f"Le sentiment prédit est : *{sentiment}*.")
             st.session_state['sentiment'] = sentiment
+            st.write(f"Le sentiment prédit est : *{sentiment}*.")
         else:
             st.write("Erreur dans la requête.")
             st.session_state['analyze_button_clicked'] = False
     else:
         st.write("Entrez s'il-vous-plaît le texte dont vous souhaitez analyser le sentiment.")
 
-if st.session_state['analyze_button_clicked']:
+if st.session_state['analyze_button_clicked'] and not st.session_state['feedback_submitted']:
     feedback = st.radio("Le sentiment prédit était-il correct ?", ["Oui", "Non"], key='feedback_radio')
     
     if st.button("Soumettre le retour"):
@@ -60,6 +58,14 @@ if st.session_state['analyze_button_clicked']:
             logger.warning("User feedback", extra=feedback_data)
             st.write("Merci pour votre retour !")
         
+        # Mark feedback as submitted
+        st.session_state['feedback_submitted'] = True
         # Reset session state after feedback submission
-        reset_session()
+        st.experimental_rerun()
+
+# Reset session state when feedback is submitted
+if st.session_state['feedback_submitted']:
+    if st.button("Réinitialiser"):
+        for key in st.session_state.keys():
+            del st.session_state[key]
         st.experimental_rerun()
