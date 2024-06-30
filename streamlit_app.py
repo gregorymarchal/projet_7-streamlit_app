d44bed367e339d8553a9f7ea02f2945f72665ed1
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-06-30 10:36:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import logging
 
 # Set up logging to Azure Application Insights
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 logger.addHandler(AzureLogHandler(connection_string="InstrumentationKey=55f8c386-676e-4155-b920-c470270eb854;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=499673b2-6e2a-4c76-a53c-2ad71070f331"))
 
 st.title("Projet 7 : Réalisez une analyse de sentiments grâce au Deep Learning")
@@ -37,7 +36,7 @@ if st.button("Analyser"):
                     "feedback": feedback
                 }
                 # Send feedback to Azure Application Insights
-                logger.info("User feedback", extra=feedback_data)
+                logger.warning("User feedback", extra=feedback_data)
                 st.write("Merci pour votre retour !")
 
         else:
