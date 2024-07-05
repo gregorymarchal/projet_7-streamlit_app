9a820660f8c4c0259a95ee7f0ba26cee0792bef6
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-07-05 10:57:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,23 @@ import requests
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 import logging
 
+# Connection string for Azure Application Insights
 connection_string = "InstrumentationKey=55f8c386-676e-4155-b920-c470270eb854;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=499673b2-6e2a-4c76-a53c-2ad71070f331"
 
 # Set up logging to Azure Application Insights
 logger = logging.getLogger(__name__)
 logger.addHandler(AzureLogHandler(connection_string=connection_string))
 
+# Streamlit application title
 st.title("Projet 7 : Réalisez une analyse de sentiments grâce au Deep Learning")
 
+# Text area for user input
 text_input = st.text_area("Entrez le texte dont vous souhaitez analyser le sentiment :")
 
+# Analyze button
 if st.button("Analyser"):
     if text_input:
-        # Remplacer l'URL par celle de votre backend FastAPI Azure
+        # Replace the URL with your FastAPI backend on Azure
         url = "https://api-projet-7.azurewebsites.net/predict"
         response = requests.post(
             url,
@@ -28,6 +32,7 @@ if st.button("Analyser"):
             sentiment = "positif" if predicted_class_id == 1 else "négatif"
             st.write(f"Le sentiment prédit est : *{sentiment}*.")
 
+            # Feedback request
             st.write("Le sentiment prédit était-il correct ?")
             col1, col2 = st.columns(2)
             with col1:
