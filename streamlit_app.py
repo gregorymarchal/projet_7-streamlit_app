763dfc478cec53862e981bfe424ded0d0ca0bd04
gregorymarchal <114147362+gregorymarchal@users.noreply.github.com>
2024-07-07 10:55:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,5 @@
 import streamlit as st
 import requests
-from opencensus.ext.azure.log_exporter import AzureLogHandler
-import logging
-import time
-
-# Connection string for Azure Application Insights
-connection_string = "InstrumentationKey=55f8c386-676e-4155-b920-c470270eb854;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=499673b2-6e2a-4c76-a53c-2ad71070f331"
-
-# Set up logging to Azure Application Insights
-logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string=connection_string))
 
 # Streamlit application title
 st.title("Projet 7 : Réalisez une analyse de sentiments grâce au Deep Learning")
@@ -20,7 +10,7 @@ text_input = st.text_area("Entrez le texte dont vous souhaitez analyser le senti
 # Analyze button
 if st.button("Analyser"):
     if text_input:
-        # Replace the URL with your FastAPI backend on Azure
+        # Replace the URL with your Flask backend on Azure
         url = "https://api-projet-7.azurewebsites.net/predict"
         response = requests.post(
             url,
@@ -42,10 +32,17 @@ if st.button("Analyser"):
                     "predicted_sentiment": sentiment,
                     "feedback": "Non"
                 }
-                # Send feedback to Azure Application Insights
-                logger.warning("User feedback", extra=feedback_data)
-                st.write("Merci pour votre retour !")
-                st.balloons()
+                # Send feedback to Flask backend
+                feedback_url = "https://api-projet-7.azurewebsites.net/feedback"
+                feedback_response = requests.post(
+                    feedback_url,
+                    json=feedback_data,
+                    headers={"Content-Type": "application/json"},
+                )
+                if feedback_response.status_code == 200:
+                    st.write("Merci pour votre retour !")
+                else:
+                    st.write("Erreur dans l'envoi du feedback.")
         else:
             st.write("Erreur dans la requête.")
     else:
